a8945da23feea92f1258751b9bf68ad09df41713
谭骏 <32965926@qq.com>
2019-04-02 14:50:20 +0800

    commit website to github.

---
+++ af_web/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using af_web.Models;
+
+namespace af_web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            //return Redirect("https://github.com/AirFleetTeam");
+            return View();
+        }
+
+        [HttpGet("{AF-XLiteEx}")]
+        public IActionResult AF_XLiteEx()
+        {
+            return Redirect("https://github.com/AirFleetTeam/AF-XLiteEx");
+        }
+
+        [HttpGet("{SG325}")]
+        public IActionResult SG325()
+        {
+            return Redirect("https://github.com/AirFleetTeam/SG325");
+        }
+    }
+}
